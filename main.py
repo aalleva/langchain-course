84d793ab2bfdce4200fa2b88dbabffb7216693a3
Andres Alleva <andres.alleva@gmail.com>
2026-02-04 16:27:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# from langchain_openai import ChatOpenAI
+# llm = ChatOpenAI(model="gpt-4o-mini")
+
+
+def main():
+    print("Hello from langchain-course!")
+
+
+if __name__ == "__main__":
+    main()
